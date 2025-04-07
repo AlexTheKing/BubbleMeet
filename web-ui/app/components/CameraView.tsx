895da2380a1cbfdddb5352059a48ccef54da153c
AlexTheKing <alexbeytuk@gmail.com>
2025-04-07 20:44:31 +0300

    Added loading state for camera view

--- web-ui/app/components/CameraView.tsx
+++ web-ui/app/components/CameraView.tsx
@@ -2,14 +2,19 @@ import { useMemo, useRef, useState } from "react";
 import { FaMicrophoneSlash } from "react-icons/fa";
 
 
-export default function CameraView({isLocal, stream, isVideoEnabled, isAudioEnabled}: {isLocal: boolean, stream: MediaStream | null, isVideoEnabled: boolean, isAudioEnabled: boolean}) {
+export default function CameraView({isLocal, stream, isVideoEnabled, isAudioEnabled}: {isLocal: boolean, stream: MediaStream | null, isVideoEnabled: boolean, isAudioEnabled: boolean}) {    
+    const [isMetadataLoaded, setIsMetadataLoaded] = useState(false);
+
     const memoizedVideo = useMemo(() => {
         return (
             <video
                 ref={(ref: HTMLVideoElement) => {
                     if (ref && stream !== null) {
                         ref.srcObject = stream;
-                        ref.onloadedmetadata = () => ref.play();
+                        ref.onloadedmetadata = () => {
+                            ref.play();
+                            setIsMetadataLoaded(true);
+                        };
                     }
                 }}
                 autoPlay={true}
@@ -23,7 +28,13 @@ export default function CameraView({isLocal, stream, isVideoEnabled, isAudioEnab
         <>
             {
                 isVideoEnabled ? (
-                    memoizedVideo
+                    <>
+                        {memoizedVideo}
+                        <div hidden={isMetadataLoaded} className="absolute inset-0 w-full h-full bg-gray-700 flex items-center justify-center">
+                            <span className="text-white">Loading...</span>
+                        </div>
+                    </>
+                    
                 ) : (
                     <div className="w-full h-full bg-gray-700 flex items-center justify-center">
                         <span className="text-white">Video Off</span>
