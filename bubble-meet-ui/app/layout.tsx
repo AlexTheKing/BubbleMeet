b67556cbcf2f57fa8e7ac4707bf2d5b0ad5a64e7
AlexTheKing <alexbeytuk@gmail.com>
2025-04-08 13:42:48 +0300

    Renamed FE folder

--- bubble-meet-ui/app/layout.tsx
+++ bubble-meet-ui/app/layout.tsx
@@ -5,7 +5,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "VideoRTC",
+  title: "Bubble Meet",
   description: "App for video conferencing",
 };
 
